aa1dd61f0f125dc5c48d57a4416a8b9558764831
polokrf <polokkumar9030@gmail.com>
2025-11-09 00:43:02 +0600

    add foter and header

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Header from './Header';
+import { Outlet } from 'react-router';
+import Footer from './Footer';
 
 const Root = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col  justify-center min-h-screen'>
+      <div>
+        <Header></Header>
+      </div>
+      <div className='flex-1'>
+        <Outlet></Outlet>
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
     </div>
   );
 };
