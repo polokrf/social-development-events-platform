2153c32e0ce8adf567a021cea2b012d93dc4b52f
polokrf <polokkumar9030@gmail.com>
2025-11-09 04:11:51 +0600

    add register function and login function

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router';
 
 const Login = () => {
+  const [show, setShow] = useState(false);
+
+  const handelLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+  }
+   const handleShow = e => {
+     e.preventDefault();
+     setShow(!show);
+   };
   return (
-    <div>
-      
+    <div className="flex flex-col justify-center min-h-screen items-center">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <div className="mt-4 text-center">
+          <h1 className="text-3xl font-bold">Login now!</h1>
+        </div>
+        <div className="card-body">
+          <form onSubmit={handelLogin}>
+            <fieldset className="fieldset">
+              <label className="label">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="input"
+                placeholder="Email"
+                
+              />
+              <label className="label">Password</label>
+              <div className=' relative'>
+                 <input
+                    type={show ?'text':'password'}
+                    name='password'
+                     className="input"
+                     placeholder="Password"
+                      />
+                    <div className=' absolute top-2 right-5'>
+                    <button onClick={handleShow} className='btn btn-xs'>
+                       {
+                        show ?<IoIosEye />:<IoIosEyeOff></IoIosEyeOff>
+                        }
+                    </button>
+                    </div>
+                </div>
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn btn-outline btn-success  my-4">
+                Login
+              </button>
+            </fieldset>
+            <p>
+              Do not have an account{' '}
+              <Link className="font-bold text-sky-600" to="/register">
+                Register
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Page/Register.jsx
+++ src/Page/Register.jsx
@@ -1,9 +1,116 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IoIosEye, IoIosEyeOff } from 'react-icons/io';
+import { Link } from 'react-router';
+import useAuth from '../Axios/useAuth';
+import { toast } from 'react-toastify';
 
 const Register = () => {
+  const { register, updateUser } = useAuth();
+  const [show, setShow] = useState(false);
+
+  const handelRegister = (e) => {
+    e.preventDefault();
+
+    const target = e.target;
+    const name = target.name.value;
+    const photo = target.photo.value;
+    const email = target.email.value;
+    const password = target.password.value;
+
+    const passwordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+
+    if (!passwordValid.test(password)) {
+       return toast.error('Password must have 6+ chars, 1 uppercase, 1 lowercase, 1 number & 1 special.');
+    }
+
+
+    const person = {
+      displayName: name,
+      photoURL:photo
+    }
+
+    register(email, password)
+      .then(res => {
+        toast.success('Successful')
+        updateUser(person)
+      }).catch(err => {
+      toast.error(err.message)
+    })
+
+   
+  }
+  
+  const handleShow = (e) => {
+    e.preventDefault()
+    setShow(!show)
+  }
   return (
-    <div>
-      
+    <div className="flex flex-col justify-center min-h-screen items-center">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <div className="mt-4 text-center">
+          <h1 className="text-3xl font-bold">Register now!</h1>
+        </div>
+        <div className="card-body">
+          <form onSubmit={handelRegister}>
+            <fieldset className="fieldset">
+              {/* name */}
+              <label className="label">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="input"
+                placeholder="Name"
+                required
+              />
+              {/* photo  */}
+              <label className="label">Photo_URL</label>
+              <input
+                type="text"
+                name="photo"
+                className="input"
+                placeholder="Photo_URL"
+                required
+              />
+              {/* email */}
+              <label className="label">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="input"
+                placeholder="Email"
+              />
+              {/* password
+               */}
+              <label className="label">Password</label>
+              <div className=' relative'>
+                <input
+                  type={show ?'text':'password'}
+                  name='password'
+                  className="input"
+                  placeholder="Password"
+                />
+                <div className=' absolute top-2 right-5'>
+                  <button onClick={handleShow} className='btn btn-xs'>
+                    {
+                      show ?<IoIosEye />:<IoIosEyeOff></IoIosEyeOff>
+                    }
+                  </button>
+                </div>
+              </div>
+
+              <button className="btn btn-outline btn-success  my-4 ">
+                Register
+              </button>
+            </fieldset>
+            <p>
+              Already have an account{' '}
+              <Link className="font-bold text-sky-600" to="/register">
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
